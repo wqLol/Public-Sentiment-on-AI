fd832cd0cee20b38f01b69d6a8c71286143b26de
wqLol <aaravgaurx@gmail.com>
2025-06-18 01:27:03 -0700

    Added Single Request

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import requests
+import requests as rq
+import json
+from apikey import key
+# get videos first
+url = r'https://www.googleapis.com/youtube/v3/search'
+params = {
+    "part": "snippet",
+    "q": "test",
+    "type": "video",
+    "maxResults": 5,
+    "order": "date",
+    "key": key
+}
+
+req = rq.get(url,params=params)
+print(req.url)
+with open('test.txt', 'w') as f:
+    f.write(str(req.content))  
