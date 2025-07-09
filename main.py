1812a194defd1eb8b620e0d8344a0afb0566ab90
wqLol <aaravgaurx@gmail.com>
2025-07-09 00:51:01 -0700

    bugfix

--- main.py
+++ main.py
@@ -8,16 +8,31 @@ startYear = 2012
 startMonth = 1
 ctime = datetime.datetime(startYear, startMonth, 1)
 
-for year in range(2012,2014):
+# for year in range(2012,2014):
+#     for month in range(1, 13):
+#         cf = open('out.json', 'r')
+#         data = json.load(cf)
+#         cf.close()
+#         ftime = ctime + dateutil.relativedelta.relativedelta(months=1)
+#         print(ctime)
+#         with open('out.json', 'w') as f:
+#             gdata = getVideo('fortnite', ctime.isoformat() + 'Z', ftime.isoformat() + 'Z')
+#             data |= gdata
+#             f.write(json.dumps(data))
+#         ctime = ftime
+
+cf = open('out.json', 'r')
+data = json.load(cf)
+cf.close()
+for year in range(2012,2013):
     for month in range(1, 13):
-        cf = open('out.json', 'r')
-        data = json.load(cf)
-        cf.close()
         ftime = ctime + dateutil.relativedelta.relativedelta(months=1)
-        print(ctime.now())
-        with open('out.json', 'w') as f:
-            gdata = getVideo('fortnite', ctime.isoformat() + 'Z', ftime.isoformat() + 'Z')
-            data |= gdata
-            f.write(json.dumps(data))
+        print(ctime)
+
+        gdata = getVideo('fortnite', ctime.isoformat() + 'Z', ftime.isoformat() + 'Z')
+        data["items"] += gdata["items"]
+
         ctime = ftime
+with open('out.json','w') as f:
+    f.write(json.dumps(data))
 # print(test)
