81b876ff46c8d376255056ee2e336d78fed6db38
wqLol <aaravgaurx@gmail.com>
2025-07-08 22:58:34 -0700

    testing

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from getVideos import getVideo
-import datetime, json
+import datetime, dateutil, json
 
 
 
-json.loads('{}')
 
-for year in range(2015,2016):
-    for month in range(1, 3):
+startYear = 2012
+startMonth = 1
+ctime = datetime.datetime(startYear, startMonth, 1)
+
+for year in range(2012,2014):
+    for month in range(1, 13):
         cf = open('out.json', 'r')
         data = json.load(cf)
         cf.close()
+        ftime = ctime + dateutil.relativedelta.relativedelta(months=1)
+        print(ctime.now())
         with open('out.json', 'w') as f:
-
-            startDate = datetime.datetime(year, month, 1).isoformat() + 'Z'
-            endDate = datetime.datetime(year, month, 1).isoformat() + 'Z'
-
-            gdata = getVideo('fortnite', startDate, endDate)
+            gdata = getVideo('fortnite', ctime.isoformat() + 'Z', ftime.isoformat() + 'Z')
             data |= gdata
             f.write(json.dumps(data))
+        ctime = ftime
 # print(test)
