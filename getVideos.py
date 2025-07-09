a230fec4426d977cbb5fe79450688f9aba692657
wqLol <aaravgaurx@gmail.com>
2025-07-09 01:29:34 -0700

    optimization

--- getVideos.py
+++ getVideos.py
@@ -1,23 +1,13 @@
 # REFERENCE - GOOGLE API DOCS
-import os
 import googleapiclient.discovery
-from apikey import key
-
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 
-
-def getVideo(query, startDate, endDate):
-    api_service_name = "youtube"
-    api_version = "v3"
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey=key)
+def getVideo(youtube, query, startDate, endDate):
 
     request = youtube.search().list(
         part="snippet",
         maxResults=25,
+        type="video",
         publishedAfter=startDate,
         publishedBefore=endDate,
         q=query
@@ -25,3 +15,18 @@ def getVideo(query, startDate, endDate):
     response = request.execute()
 
     return response
+
+
+
+def getComments(youtube, videoId):
+
+    request = youtube.comments().list(
+        part="snippet",
+        parentId = videoId
+    )
+    
+    response = request.execute()
+
+    return response
+
+
--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from getVideos import getVideo
 import datetime, dateutil, json
-
-
+import googleapiclient.discovery
+from apikey import key
 
 
 startYear = 2012
 startMonth = 1
 ctime = datetime.datetime(startYear, startMonth, 1)
 
-# for year in range(2012,2014):
-#     for month in range(1, 13):
-#         cf = open('out.json', 'r')
-#         data = json.load(cf)
-#         cf.close()
-#         ftime = ctime + dateutil.relativedelta.relativedelta(months=1)
-#         print(ctime)
-#         with open('out.json', 'w') as f:
-#             gdata = getVideo('fortnite', ctime.isoformat() + 'Z', ftime.isoformat() + 'Z')
-#             data |= gdata
-#             f.write(json.dumps(data))
-#         ctime = ftime
+api_service_name = "youtube"
+api_version = "v3"
+
+youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=key)
 
 cf = open('out.json', 'r')
 data = json.load(cf)
@@ -29,7 +22,7 @@ for year in range(2012,2014):
         ftime = ctime + dateutil.relativedelta.relativedelta(months=1)
         print(ctime)
 
-        gdata = getVideo('', ctime.isoformat() + 'Z', ftime.isoformat() + 'Z')
+        gdata = getVideo(youtube,'', ctime.isoformat() + 'Z', ftime.isoformat() + 'Z')
         data["items"] += gdata["items"]
 
         ctime = ftime
