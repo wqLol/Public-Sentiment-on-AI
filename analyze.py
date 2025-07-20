159d7395f3ff47e4c682505dc5428fbfb27899ed
wqLol <aaravgaurx@gmail.com>
2025-07-19 17:21:41 -0700

    analyzing changes + downgrading python to 3.9.13

--- analyze.py
+++ analyze.py
@@ -1,28 +1,27 @@
-# from transformers import pipeline
+from transformers import pipeline
 import json, torch
-
+ 
 print(torch.cuda.is_available())
-# classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english", device = 0)
 
-# f = open('comments.json')
-# data = json.load(f)
-# f.close()
-# x = 1
-# total = 0
-# for vid in data["data"]:
-#     for commentResource in vid["items"]:
-#         comment = commentResource["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
-#         if not comment: continue
-#         try:
-#             val = classifier(comment[:512])[0]
-#         except RuntimeError as e:
-#             print(e)
-#             continue
-#         total += val["score"] if val["label"] == "POSITIVE" else -1 * val["score"]
-        
-#         print(comment[:512] + ' ' + str(val))
-#         x += 1
-        
-        
-# print("\n" + total/x)
+classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english", device = 0)
 
+f = open('comments.json')
+data = json.load(f)
+f.close()
+total = 0
+
+comments = []
+for vid in data["data"]:
+    for commentResource in vid["items"]:
+        comment = commentResource["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
+        if comment: 
+            comments.append(comment[:512])
+        
+
+results = classifier(comments)
+
+
+for val in results:
+    total += val["score"] if val["label"] == "POSITIVE" else -1 * val["score"]
+
+print("\n" + total/len(results))
