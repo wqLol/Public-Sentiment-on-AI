dc2147b63ee3ea919c850e0edb9b1c898a30190c
wqLol <aaravgaurx@gmail.com>
2025-07-20 00:02:16 -0700

    working avg sentiment

--- analyze.py
+++ analyze.py
@@ -15,13 +15,13 @@ for vid in data["data"]:
     for commentResource in vid["items"]:
         comment = commentResource["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
         if comment: 
-            comments.append(comment[:512])
+            comments.append(comment)
         
 
-results = classifier(comments)
+results = classifier(comments, truncation=True)
 
 
 for val in results:
     total += val["score"] if val["label"] == "POSITIVE" else -1 * val["score"]
 
-print("\n" + total/len(results))
+print("\n" + str(total/len(results)))
