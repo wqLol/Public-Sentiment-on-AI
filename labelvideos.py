01bbe580f8c8f89f49c2965e61ee254611306505
wqLol <aaravgaurx@gmail.com>
2025-07-20 18:05:48 -0700

    some labeling

--- labelvideos.py
+++ labelvideos.py
@@ -7,8 +7,9 @@ f.close()
 
 for x in range(len(data["data"])):
     print(data["data"][x][0])
-    
-    
+    v =  input("\n")
+    if v == "d": break
+    data["data"][x][2]["relevanceLabel"] == int(v)
     
     
 
